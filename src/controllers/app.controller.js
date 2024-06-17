d962f9e4dd6657900a3dfbd4457facc8ac062724
Hongsonnguyen <hongsonnguyen1003@gmail.com>
2024-06-17 23:10:00 +0700

    secure true

--- src/controllers/app.controller.js
+++ src/controllers/app.controller.js
@@ -6,7 +6,7 @@ import appService from '../services/app.service'
 require('dotenv').config()
 const SECRET = process.env.SECRET
 const MAX_AGE = process.env.MAX_AGE
-const secure = false
+const secure = true
 const sameSite = 'Lax'
 const httpOnly = false
 
